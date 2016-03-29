c95ef1d1300969e502aa6ffe42d3219603da75e7
msp4096 <msp4096@yahoo.com>
2016-03-29 14:29:11 -0400

    fixed paths again

--- powerMonitoring/meter_read.sh
+++ powerMonitoring/meter_read.sh
@@ -5,12 +5,12 @@
 sleep 3
 
 PID=$!
-#path="/home/pi/powerMonitoring"
-#logpath="/home/pi/powerMonitoring/reading"
+#path="/home/pi/projects/powerMonitoring"
+#logpath="/home/pi/projects/powerMonitoring/reading"
 #meterid=4410055
 
-/home/pi/powerMonitoring/rtlamr -filterid=4410055 -logfile="/home/pi/powerMonitoring/reading" -quiet -single=true -format="json"
+/home/pi/projects/powerMonitoring/rtlamr -filterid=4410055 -logfile="/home/pi/projects/powerMonitoring/reading" -quiet -single=true -format="json"
 
-python /home/pi/powerMonitoring/energy_parse.py
+python /home/pi/projects/powerMonitoring/energy_parse.py
 kill -9 $PID
 
