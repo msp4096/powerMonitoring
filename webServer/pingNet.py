8d158a3c65fe8288a05aa8d39ac8629ffb6eb3dd
msp4096 <msp4096@yahoo.com>
2016-03-30 14:37:37 -0400

    added dynamic reading of python dict for IP addresses, and color-coded table

--- webServer/pingNet.py
+++ webServer/pingNet.py
@@ -1,55 +1,31 @@
-import sys
-import os
+def pingNet():
+    import sys
+    import os
 
-router = '192.168.0.1'
-lcom   = '192.168.0.105'
-ubnt   = '192.168.0.77'
-seccam = '192.168.0.88'
-HPprint= '192.168.0.112'
-#nanaph = '192.168.0.115' #these won't ping...
-#myphon = '192.168.0.101' #these won't ping...
+    devices = {
+        'Router'      : {'IP': '192.168.0.1'   },
+        'DoomAlpha'   : {'IP': '192.168.0.105' },
+        "Nana's UBNT" : {'IP': '192.168.0.77'  },
+        'Cameras'     : {'IP': '192.168.0.88'  },
+        'HPprinter'   : {'IP': '192.168.0.112' }
+    }
 
-router_response = os.system("ping -c 1 " + router)
-lcom_response   = os.system("ping -c 1 " + lcom)
-ubnt_response   = os.system("ping -c 1 " + ubnt)
-seccam_response = os.system("ping -c 1 " + seccam)
-HPprint_response= os.system("ping -c 1 " + HPprint)
-#nanaph_response = os.system("ping -c 1 " + nanaph)
-#myphon_response = os.system("ping -c 1 " + myphon)
+    print 'original dict'
+    print devices
 
-#and then check the response...
-if router_response == 0:
-  print 'Router is up'
-  router_response = 'Up'
-else:
-  print 'Router is down'
-  router_response = 'Down'
+    for key in devices:
 
-if lcom_response == 0:
-  print 'DoomAlpha is up'
-  lcom_response = 'Up'
-else:
-  print 'DoomAlpha is down'
-  lcom_response = 'Down'
+        if os.system("ping -c 1 " + devices[key]['IP']) == 0:
+            devices[key]['Status'] = 'up'
+        else:
+            devices[key]['Status'] = 'down'
 
-if ubnt_response == 0:
-  print 'Nana UBNT is up'
-  ubnt_response = 'Up'
-else:
-  print 'Nana UBNT is down'
-  ubnt_response = 'Down'
+    print 'new dict'
+    print devices
 
-if seccam_response == 0:
-  print 'Cameras are up'
-  seccam_response = "Up"
-else:
-  print 'Cameras are down'
-  seccam_response = "Down"
+    return devices
 
-if HPprint_response == 0:
-    print 'Printer is up'
-    HPprint_response = "Up"
-else:
-    print 'Printer is down'
-    HPprint_response = "Down"
-#return {router_response, lcom_response, ubnt_response, seccam_response, HPprint_response}
+
+
+if __name__ == "__main__":
+    pingNet()
--- webServer/webserver.py
+++ webServer/webserver.py
@@ -31,58 +31,58 @@ def get_kWh():
 
 #start the server and get values when page is refreshed
 def application(environ, start_response):
-    #network stuff
-    router = '192.168.0.1'
-    lcom   = '192.168.0.105'
-    ubnt   = '192.168.0.77'
-    seccam = '192.168.0.88'
-    HPprint= '192.168.0.112'
-    #nanaph = '192.168.0.115' #these won't ping...
-    #myphon = '192.168.0.101' #these won't ping...
-
-    router_response = os.system("ping -c 1 " + router)
-    lcom_response   = os.system("ping -c 1 " + lcom)
-    ubnt_response   = os.system("ping -c 1 " + ubnt)
-    seccam_response = os.system("ping -c 1 " + seccam)
-    HPprint_response= os.system("ping -c 1 " + HPprint)
-    #nanaph_response = os.system("ping -c 1 " + nanaph)
-    #myphon_response = os.system("ping -c 1 " + myphon)
-
-    #and then check the response...
-    if router_response == 0:
-      print 'Router is up'
-      router_response = 'Up'
-    else:
-      print 'Router is down'
-      router_response = 'Down'
-
-    if lcom_response == 0:
-      print 'DoomAlpha is up'
-      lcom_response = 'Up'
-    else:
-      print 'DoomAlpha is down'
-      lcom_response = 'Down'
-
-    if ubnt_response == 0:
-      print 'Nana UBNT is up'
-      ubnt_response = 'Up'
-    else:
-      print 'Nana UBNT is down'
-      ubnt_response = 'Down'
-
-    if seccam_response == 0:
-      print 'Cameras are up'
-      seccam_response = "Up"
-    else:
-      print 'Cameras are down'
-      seccam_response = "Down"
-
-    if HPprint_response == 0:
-        print 'Printer is up'
-        HPprint_response = "Up"
-    else:
-        print 'Printer is down'
-        HPprint_response = "Down"
+    # #network stuff
+    # router = '192.168.0.1'
+    # lcom   = '192.168.0.105'
+    # ubnt   = '192.168.0.77'
+    # seccam = '192.168.0.88'
+    # HPprint= '192.168.0.112'
+    # #nanaph = '192.168.0.115' #these won't ping...
+    # #myphon = '192.168.0.101' #these won't ping...
+    #
+    # router_response = os.system("ping -c 1 " + router)
+    # lcom_response   = os.system("ping -c 1 " + lcom)
+    # ubnt_response   = os.system("ping -c 1 " + ubnt)
+    # seccam_response = os.system("ping -c 1 " + seccam)
+    # HPprint_response= os.system("ping -c 1 " + HPprint)
+    # #nanaph_response = os.system("ping -c 1 " + nanaph)
+    # #myphon_response = os.system("ping -c 1 " + myphon)
+    #
+    # #and then check the response...
+    # if router_response == 0:
+    #   print 'Router is up'
+    #   router_response = 'Up'
+    # else:
+    #   print 'Router is down'
+    #   router_response = 'Down'
+    #
+    # if lcom_response == 0:
+    #   print 'DoomAlpha is up'
+    #   lcom_response = 'Up'
+    # else:
+    #   print 'DoomAlpha is down'
+    #   lcom_response = 'Down'
+    #
+    # if ubnt_response == 0:
+    #   print 'Nana UBNT is up'
+    #   ubnt_response = 'Up'
+    # else:
+    #   print 'Nana UBNT is down'
+    #   ubnt_response = 'Down'
+    #
+    # if seccam_response == 0:
+    #   print 'Cameras are up'
+    #   seccam_response = "Up"
+    # else:
+    #   print 'Cameras are down'
+    #   seccam_response = "Down"
+    #
+    # if HPprint_response == 0:
+    #     print 'Printer is up'
+    #     HPprint_response = "Up"
+    # else:
+    #     print 'Printer is down'
+    #     HPprint_response = "Down"
 
     #solar stuff
     solar = get_solar()
@@ -94,8 +94,8 @@ def application(environ, start_response):
     print kWh
 
     #network stuff
-    #Intra_pings = pingNet()
-    #print Intra_pings
+    Intra_pings = pingNet.pingNet()
+    print Intra_pings
 
     #sense hat stuff
     sense=SenseHat()
@@ -122,18 +122,34 @@ def application(environ, start_response):
     html7 = '</td></tr></table>'
 
     table1 = html1 + html2 + str(currentPower) + html3 + str(kWh) + html4 + str(temp_f) + html5 + str(humidity) + html6 + str(pressure_in)+ html7
+    # html8 = '<h2>Network</h2>'
+    # html9 = '<table border="1"><tr><td><strong>Router</strong></td><td>'
+    # html10 = '</td></tr><tr><td><strong>HP Printer</strong></td><td>'
+    # html11 = '</td></tr><tr><td><strong>DoomAlpha</strong></td><td>'
+    # html12 = '</td></tr><tr><td><strong>Nana&#39s UBNT</strong></td><td>'
+    # html13 = '</td></tr><tr><td><strong>Cameras</strong></td><td>'
+    # html14 = '</td></tr></table>'
+    #htmltime = '<h2>Time:</h2>'
 
+    htmlclose = '</body></html>'
     html8 = '<h2>Network</h2>'
-    html9 = '<table border="1"><tr><td><strong>Router</strong></td><td>'
-    html10 = '</td></tr><tr><td><strong>HP Printer</strong></td><td>'
-    html11 = '</td></tr><tr><td><strong>DoomAlpha</strong></td><td>'
-    html12 = '</td></tr><tr><td><strong>Nana&#39s UBNT</strong></td><td>'
-    html13 = '</td></tr><tr><td><strong>Cameras</strong></td><td>'
+    html9 = '<table border="1">'
+    table_rows = ''
+#    for (host, status) in Intra_pings.items():
+#        table_rows += "<tr><td><strong>{}</strong></td><td>{}</td></tr>".format(host, status)
+
+    for key in Intra_pings:
+        if Intra_pings[key]['Status'] == 'up':
+            table_rows += '<tr><td><strong>' + key + '</strong></td><td   bgcolor="#00FF00">' + Intra_pings[key]['Status'] + "</td></tr>"
+        elif Intra_pings[key]['Status'] == 'down':
+            table_rows += '<tr><td><strong>' + key + '</strong></td><td   bgcolor="#FF0000">' + Intra_pings[key]['Status'] + "</td></tr>"
+
     html14 = '</td></tr></table>'
     #htmltime = '<h2>Time:</h2>'
     htmlclose = '</body></html>'
 
-    table2 = html8 + html9 + router_response + html10 + HPprint_response + html11 + lcom_response + html12 + ubnt_response + html13 + seccam_response+ html14
+    table2 = html8 + html9 + table_rows + html14
+    #table2 = html8 + html9 + router_response + html10 + HPprint_response + html11 + lcom_response + html12 + ubnt_response + html13 + seccam_response+ html14
 
     # response_body
     response_body = table1 + table2 + htmlclose
