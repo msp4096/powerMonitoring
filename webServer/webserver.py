bd2d5836b308822a4723ead4f137d7894e7c91bb
msp4096 <msp4096@yahoo.com>
2016-09-22 08:07:36 -0400

    added lines for current power and net usage for the month

--- webServer/webserver.py
+++ webServer/webserver.py
@@ -22,15 +22,25 @@ def get_solar():
 	f.close()
 	return json.loads(json_solar)
 
-#Get current meter reading from the log file
+#Get current information from the log file
 def get_kWh():
-    path = '/home/pi/projects/powerMonitoring/'
-    tfile = open(path+'reading')
-    text = tfile.read()
-    tfile.close()
-    data = json.loads(text)
-    kWh = float(data['Message']['Consumption']) / 100
-    return kWh
+    
+	path = '/home/pi/projects/powerMonitoring/logs/'
+	fileTS = time.strftime("%Y-%m")
+	tfile = open(path+'energy_'+fileTS+'.log')
+	lines = tfile.readlines()
+	if lines:
+		first_line = str(lines[0].rstrip())
+		last_line = str(lines[-1].rstrip())
+	tfile.close()
+	first_line = first_line.split(',')
+	last_line  = last_line.split(',')
+	print first_line
+	print last_line
+	kWh = last_line[1]
+	netuse = last_line[3]
+	pwr = last_line[2]
+	return (kWh, netuse, pwr)
 
 #start the server and get values when page is refreshed
 def application(environ, start_response):
@@ -43,12 +53,14 @@ def application(environ, start_response):
     print currentPower
 
     #meter stuff
-    kWh = get_kWh()
+    kWh, netuse, pwr = get_kWh()
     print kWh
+    print netuse
+    print pwr
 
     #network stuff
     Intra_pings = pingNet.pingNet()
-    print Intra_pings
+    #print Intra_pings
 
     #sense hat stuff
     sense=SenseHat()
@@ -69,12 +81,14 @@ def application(environ, start_response):
     html1 = '<html><header><h1>Pi Monitoring System</h1><h2>Power & Environment</h2><title>Pi in the Basement</title></header><body>'
     html2 = '<table border="1"><tr><td><strong>Current Solar Output (W)</strong></td><td>'
     html3 = '</td></tr><tr><td><strong>Meter Reading (kWh)</strong></td><td>'
+    htmlx = '</td></tr><tr><td><strong>Net this month (kWh)</strong></td><td>'
+    htmly = '</td></tr><tr><td><strong>Current power (W)</strong></td><td>' 
     html4 = '</td></tr><tr><td><strong>Basement Temp (F)</strong></td><td>'
     html5 = '</td></tr><tr><td><strong>Basement Humidity (%)</strong></td><td>'
     html6 = '</td></tr><tr><td><strong>Basement Pressure (in)</strong></td><td>'
     html7 = '</td></tr></table>'
 
-    table1 = html1 + html2 + str(currentPower) + html3 + str(kWh) + html4 + str(temp_f) + html5 + str(humidity) + html6 + str(pressure_in)+ html7
+    table1 = html1 + html2 + str(currentPower) + html3 + str(kWh) + htmlx + str(netuse) + htmly + str(pwr) + html4 + str(temp_f) + html5 + str(humidity) + html6 + str(pressure_in)+ html7
 
     htmlclose = '</body></html>'
     html8 = '<h2>Network</h2>'
@@ -91,7 +105,7 @@ def application(environ, start_response):
 
     html14 = '</td></tr></table>'
     #htmltime = '<h2>Time:</h2>'
-    html15 = '<h2>Current picture</h2><img src="http://127.0.0.1:6600" alt="Pi Camera Picture">'
+#    html15 = '<h2>Current picture</h2><img src="http://127.0.0.1:6600" alt="Pi Camera Picture">'
 #    html15 = '<h2>Current picture</h2><img src="image.jpg" alt="Pi Camera Picture">'
 #    html15 = '<h2>Current picture</h2><img src="/home/pi/projects/webServer/image.jpg" alt="Pi Camera Picture" style="width:304px;height:228px;">'
     htmlclose = '</body></html>'
@@ -99,7 +113,8 @@ def application(environ, start_response):
     table2 = html8 + html9 + table_rows + html14
 
     # response_body
-    response_body = table1 + table2 + html15 + htmlclose
+    #response_body = table1 + table2 + html15 + htmlclose
+    response_body = table1 + table2 + htmlclose
     status = '200 OK'
 
     # Some header magic, create response
