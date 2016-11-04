6abe6dfacb6b6eb0c7cd7b399ad64fd42c8b0897
msp4096 <msp4096@yahoo.com>
2016-11-04 07:54:16 -0400

    Updating to the latest and greatest logs and webserver enhancements

---
+++ powerMonitoring/network_maint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sleep 3
+
+PID=$!
+
+ssh admin@192.168.0.105
+
+
+#path="/home/pi/projects/powerMonitoring"
+#logpath="/home/pi/projects/powerMonitoring/reading"
+#meterid=4410055
+
+#/home/pi/projects/powerMonitoring/rtlamr -filterid=4410055 -logfile="/home/pi/projects/powerMonitoring/reading" -quiet -single=tru$
+
+#python /home/pi/projects/powerMonitoring/energy_parse.py
+#kill -9 $PID
+
