dfdd16daeff1a109a2eb8383821523ffc8abdec5
Eric Renda <msp4096@yahoo.com>
2021-11-15 12:34:45 -0500

    backing stuff up, yes I know this isn't a backup.

--- webServer/piCamera.py
+++ webServer/piCamera.py
@@ -1,4 +1,15 @@
-import picamera
+from picamera import PiCamera
+from time import sleep
+from sense_hat import SenseHat
 
-camera = picamera.PiCamera()
+camera = PiCamera()
+
+sense = SenseHat()
+
+white = (255,255,255)
+red  = (255,0,0)
+blue = (0,0,255)
+sense.clear(blue)
 camera.capture('image.jpg')
+sense.clear()
+exit()
